1c14661b4caf614e5674699658b91b4421595f8b
mirviseshalu <hness7683@gmail.com>
2023-08-06 22:33:27 -0700

    first commit

--- src/message.tsx
+++ src/message.tsx
@@ -2,7 +2,7 @@
 //PascalCasing
 function Message(){
   //JSX: JavaScript XML
-  const name = 'Jivin';
+  const name = 'mirvise';
   if (name)
     return   <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>
